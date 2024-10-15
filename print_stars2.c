9c1d82bdaa5f95e266172a658efaa07418534b04
dfddddfdf <kujudnwn@naver.com>
2024-10-15 12:55:34 +0900

    Add files via upload
    별을 프린트하는 프로그램입니다

---
+++ print_stars2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+
+void print_stars();
+
+int main()
+{
+    int x, y;
+    for(y =0; y<5; y++)
+
+
+{
+    for(int i=0; i<10; i++)
+      printf("*");
+    printf("\n");
+}
+
+
+    return 0;
+
+}
+
